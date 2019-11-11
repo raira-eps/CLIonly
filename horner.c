9cb6ce8811e2f7c239f84ba35b37406f63fea595
raira-eps <anhappynorthwind39@ezweb.ne.jp>
2019-11-11 16:52:49 +0900

    add time file and horner algorithms file

---
+++ horner.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+double fn(double,double*,int);
+
+void main(void)
+{
+    static double a[] = {1,2,3,4,5}; //係数
+    double x;
+    for(x = 1;x <= 5;x++)
+    {
+        printf("fn(%f)=%f\n",x,fn(x,a,4));
+    }
+}
+
+double fn(double x,double a[],int n)
+{
+    double p;
+    int i;
+
+    p = a[n];
+    for(i = n-1;i >= 0;i--)
+    {
+        p = p*x + a[i];
+    }
+     return p;
+}
