f2359a8138f57ff1746a1d320f34670160e5de78
raira-eps <anhappynorthwind39@ezweb.ne.jp>
2019-11-15 22:03:03 +0900

    add peipei file

---
+++ peipei.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int IAbs(int x);
+void Parity(int x);
+void main(void);
+
+//絶対値を返す
+int IAbs(int x)
+{
+    return ((x>= 0)? x: -x);
+}
+//奇数か偶数かを表示する
+void Parity(int x)
+{
+    printf("%s", (x%2)? "奇数": "偶数");
+}
+
+void main(void)
+{
+    int n;
+    printf("Please enter an integer");
+    scanf("%d", &n);
+    printf("The absolute value of that number is %d \n", IAbs(n));
+    printf("that is");
+    Parity(n);
+}
