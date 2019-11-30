eddf7e716884e26379d608f2bd1f84d4d5dd7763
raira-eps <anhappynorthwind39@ezweb.ne.jp>
2019-11-30 23:44:28 +0900

    add server.cs file

---
+++ server.cs
@@ -0,0 +1,28 @@
+//C#でのデータベース接続
+//基本の実装例 (写し)
+
+using System.Configuration;
+using System.Data.SqlClient;
+
+public void Connect1()
+{
+    //接続文字列の取得
+    var connectionString = ConfigurationManager.Configuration["sqlsvr"].ConnectionString;
+
+    //データベース接続の準備
+    var connection = new SqlConnection(connectionString);
+
+    //データベースの接続開始
+    connection.Open();
+
+    //実行するsqlの準備
+    var command = new SqlCommand();
+    command.Connection = connection;
+    command.CommandText = @"SELECT count(*) FROM T_USER" ;
+
+    //SQLの実行
+    command.ExecuteNonQuery();
+
+    //データベースの接続終了
+    connection.Close();
+}
